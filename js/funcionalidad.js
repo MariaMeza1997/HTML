9b70ade8ca1a3881a509f7da63f249ba71824e0e
Maria Meza <mmeza@licorne.mx>
2023-04-11 20:13:39 -0600

    ...

---
+++ js/funcionalidad.js
@@ -0,0 +1,15 @@
+/*console.warn("Advertencia")
+
+console.error("Error")*/
+
+var a =10
+var b = 5
+
+function varible(){
+    const c = 9 
+    console.log(c)
+}
+
+varible()
+
+console.log(a ** b)
