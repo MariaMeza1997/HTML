081a7c9bdf5b419528beaae40f899d66f4d2055e
Maria Meza <mmeza@licorne.mx>
2023-04-13 20:11:58 -0600

    ...

--- js/funcionalidad.js
+++ js/funcionalidad.js
@@ -2,14 +2,39 @@
 
 console.error("Error")*/
 
-var a =10
-var b = 5
+var a ="10"//string
+var b = 10 //number
+var c = true //boolean
+var d = ["Maria","Jose","Javier","Mariana","Leonora","Fany"]
+var e = new Array("Maria","Jose","Javier","Mariana","Leonora","Fany")
+const veggies = ['🥦', '🥒', '🌽', '🥕'];
+const saladCopy = [...salad, ...veggies]
+const salad = new Array('🍅', '🍄', '🥦', '🥒', '🌽', '🥕', '🥑');
+console.log()//Original
+console.log()//Agregar emoji
+console.log()//Eliminar emoji
+console.log()//tamaño
+console.log()//emoji especifico
+console.log('⛄')
+
+let [tomate, hongo, zanahoria] = ['🍅', '🍄', '🥕'];
+console.log(tomate)
+
+console.log(Array.isArray(salad))
+
+
+
+/*if(a == b){
+    console.log("Verdarero")
+}else{
+    console.log("Falso")
+}
+*/
 
 function varible(){
     const c = 9 
-    console.log(c)
+    
 }
 
 varible()
 
-console.log(a ** b)
