907aa2dfea5bb64776b8de246ed20a79d76ae2dc
Maria Meza <mmeza@licorne.mx>
2023-04-27 19:29:55 -0600

    ...

--- js/funcionalidad.js
+++ js/funcionalidad.js
@@ -7,25 +7,24 @@ var b = 10 //number
 var c = true //boolean
 
 var d = ["Maria","Jose","Javier","Mariana","Leonora","Fany"]
-var e = new Array("Maria","Jose","Javier","Mariana","Leonora","Fany")
 const veggies = ['🥦', '🥒', '🌽', '🥕','🥦'];
 const salad = new Array('🍅', '🍄', '🥦', '🥒', '🌽', '🥕', '🥑');
+
+
 const saladCopy = [...salad, ...veggies]
-
-
 
 let [tomate, hongo, zanahoria] = ['🍅', '🍄', '🥕'];
 
 //var calificacion = a === b ?  console.log("Verdarero") : console.log("Falso");
 
 if ( veggies.length >0 ){
-
-    console.log("Dentro de la condicional ")
     
-    var i = 0 
+    var i = 0
 
     // Condición: Mientras la variable contador sea menor de 5
-   /* while (i <= veggies.length) {
+    do {
+
+    console.log("Dentro de la condicional ")
 
       switch(veggies[i]){
         case '🥦':
@@ -47,9 +46,14 @@ if ( veggies.length >0 ){
     }
     
       i = i + 1; // Incrementamos el valor de i
-    }*/
+    }while(i <= veggies.length)
 
-    for( i ; i <= veggies.length; i = i + 1 ){
+
+    //console.log("what's your name!");
+     //var x = readline();
+     //console.log("Hi ! "+input);
+
+    /*for( i ; i <= veggies.length; i = i + 1 ){
         
         switch(veggies[i]){
             case '🥦':
@@ -70,7 +74,28 @@ if ( veggies.length >0 ){
     
         }
 
-    }
+    }*/
    
 
 }
+
+
+function ejercicio1(){
+    var e = new Array()
+
+    if(e.length !== 0){
+        console.log("%c¡ESTE ARREGLO NO ESTA VACIO", 
+        "background:red; color:#fff; padding: 5px 10px;")
+
+    }else{
+        console.log("%c¡ESTE ARREGLO ESTA VACIO", 
+        "background:red; color:#fff; padding: 5px 10px;")
+        //var e = ["Maria","Jose","Javier","Mariana","Leonora","Fany" ]
+        var e = new Array("Maria","Jose","Javier","Mariana","Leonora","Fany")
+    }
+
+    console.log(e)
+
+}
+
+ejercicio1()
