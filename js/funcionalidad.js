5a37962257f0f12f552ba47ce4dbb9b70c09352d
Maria Meza <mmeza@licorne.mx>
2023-05-03 19:10:46 -0600

    ...

--- js/funcionalidad.js
+++ js/funcionalidad.js
@@ -1,101 +1,18 @@
-/*console.warn("Advertencia")
+const button = document.querySelector('.landing_page_box_one')
+const modal = document.querySelector('.landing_page_modal')
+const deleteModal = document.querySelector('.delete')
 
-console.error("Error")*/
-var i = 0 
-var a =9 //string
-var b = 10 //number
-var c = true //boolean
+function abrir () {
 
-var d = ["Maria","Jose","Javier","Mariana","Leonora","Fany"]
-const veggies = ['🥦', '🥒', '🌽', '🥕','🥦'];
-const salad = new Array('🍅', '🍄', '🥦', '🥒', '🌽', '🥕', '🥑');
-
-
-const saladCopy = [...salad, ...veggies]
-
-let [tomate, hongo, zanahoria] = ['🍅', '🍄', '🥕'];
-
-//var calificacion = a === b ?  console.log("Verdarero") : console.log("Falso");
-
-if ( veggies.length >0 ){
-    
-    var i = 0
-
-    // Condición: Mientras la variable contador sea menor de 5
-    do {
-
-    console.log("Dentro de la condicional ")
-
-      switch(veggies[i]){
-        case '🥦':
-            console.log("brocoli")
-        break;
-
-        case '🥒':
-            console.log("pepino")
-        break;
-
-        case '🌽':
-            console.log("elote")
-        break;
-
-        case '🥕':
-            console.log("zanahoria")
-        break;
-
-    }
-    
-      i = i + 1; // Incrementamos el valor de i
-    }while(i <= veggies.length)
-
-
-    //console.log("what's your name!");
-     //var x = readline();
-     //console.log("Hi ! "+input);
-
-    /*for( i ; i <= veggies.length; i = i + 1 ){
-        
-        switch(veggies[i]){
-            case '🥦':
-                console.log("brocoli")
-            break;
-    
-            case '🥒':
-                console.log("pepino")
-            break;
-    
-            case '🌽':
-                console.log("elote")
-            break;
-    
-            case '🥕':
-                console.log("zanahoria")
-            break;
-    
-        }
-
-    }*/
-   
+    modal.classList.toggle('landing_page_modal--active')
 
 }
 
+function closeModal () {
 
-function ejercicio1(){
-    var e = new Array()
-
-    if(e.length !== 0){
-        console.log("%c¡ESTE ARREGLO NO ESTA VACIO", 
-        "background:red; color:#fff; padding: 5px 10px;")
-
-    }else{
-        console.log("%c¡ESTE ARREGLO ESTA VACIO", 
-        "background:red; color:#fff; padding: 5px 10px;")
-        //var e = ["Maria","Jose","Javier","Mariana","Leonora","Fany" ]
-        var e = new Array("Maria","Jose","Javier","Mariana","Leonora","Fany")
-    }
-
-    console.log(e)
+    modal.classList.remove('landing_page_modal--active')
 
 }
 
-ejercicio1()
+deleteModal.addEventListener('dblclick', closeModal )
+button.addEventListener('click', abrir)
