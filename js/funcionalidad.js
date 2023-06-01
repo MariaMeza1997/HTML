e68fc3e340486a33c3dff4679145c1e56c44a117
Maria Meza <mmeza@licorne.mx>
2023-05-31 20:05:26 -0600

    ...

--- js/funcionalidad.js
+++ js/funcionalidad.js
@@ -1,18 +1,43 @@
-const button = document.querySelector('.landing_page_box_one')
-const modal = document.querySelector('.landing_page_modal')
-const deleteModal = document.querySelector('.delete')
+var num1 = 10;
+var num2 = 12;
+var num3 = 20;
 
-function abrir () {
+//*Operadores aritmeticos
+//*Suma
+var Result = num1 + num1;
+console.log("Suma de a + b: " + Result)
 
-    modal.classList.toggle('landing_page_modal--active')
+//*Resta
+Result= num2 - num1;
+console.log("Resta de a + b: " + Result)
 
-}
+//*Multiplicacion
+Result = num1 * num3;
+console.log("Multiplicacion de a + b: " + Result)
 
-function closeModal () {
+//*Division
+Result = num3 / num1;
+console.log("Division de a + b: " + Result)
 
-    modal.classList.remove('landing_page_modal--active')
+//*Modulo 
+Result = num3 % num1;
+console.log("Modulo de a + b: " + Result)
 
-}
+//*Exponente
+Result = num1 ** 2;
+console.log("Exponente de a + b: " + Result)
 
-deleteModal.addEventListener('dblclick', closeModal )
-button.addEventListener('click', abrir)
+
+/*
+Tenemos tres numeros 
+
+a= 20 , b = 3 c =24
+
+Nota: Todo tiene que llkevar comentario e impresion en console 
+
+1) Suma de los tres numeros
+2)La resta de (a+b)-c
+3)Multiplicacion de a x b x b x c
+4)Modulo de a / b
+5) Eleva c a la quinta potencia 
+*/
